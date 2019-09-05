301dbcc823f4860ab7480a308c92f0fdd77d19a8
laurenlittle <littlelauren016@gmail.com>
2019-09-04 20:58:31 -0400

    add to form to Signup component

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,5 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+import Layout from '../core/Layout';
+import { API } from '../config';
 
-const Signup = () => <h2>Signup Component</h2>;
+
+const Signup = () => {
+
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: false
+  })
+
+  const { name, email, password } = formValues;
+
+  const handleChange = fieldName => event => {
+    setFormValues({ ...formValues, error: false, [fieldName]: event.target.value })
+  }
+
+  const signup = (user) => {
+    // console.log({ name, email, password });
+    return fetch(`${API}/signup`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify(user) // convert JS object passed to JSON
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    })
+
+  };
+
+  const handleClickSubmit = e => {
+    e.preventDefault();
+    signup({ name, email, password }); // user object in method as JS object
+  }
+
+  const signUpForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Name</label>
+        <input
+          className='form-control'
+          type='text'
+          value={name}
+          onChange={handleChange('name')}
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          className='form-control'
+          type='text'
+          value={email}
+          onChange={handleChange('email')}
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          className='form-control'
+          type='text'
+          value={password}
+          onChange={handleChange('password')}
+        />
+      </div>
+
+      <button onClick={handleClickSubmit} className='btn btn-primary'>Submit</button>
+    </form>
+  );
+
+ return (
+  <Layout title='Sign Up' description='Get started Today' className='container col-md-8 offset-md-2'>
+    {signUpForm()}
+    {/* {JSON.stringify(formValues)} */}
+  </Layout>
+ )
+};
 
 export default Signup;
