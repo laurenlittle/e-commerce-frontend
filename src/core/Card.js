57efb9f5387f8d18dadf3d60a5649507d43725f7
laurenlittle <littlelauren016@gmail.com>
2019-09-20 06:57:15 -0400

    created getProducts, loadProductsSold, loadProductsByArrival methods, display in Card Component on Home Component

---
+++ src/core/Card.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Card = ({product}) => {
+  return (
+
+    <div className='col-4 mb-3'>
+      <div className='card'>
+        <div className='card-header'>
+          {product.name}
+        </div>
+        <div className='card-body'>
+          <p> {product.description} </p>
+          <p> {product.price} </p>
+          <Link to='/'>
+            <button className='btn btn-outline-primary mb-2 mt-2 mr-4'>
+              View Product
+            </button>
+          </Link>
+          <Link to='/'>
+            <button className='btn btn-outline-warning mb-2 mt-2'>
+              Add to Cart
+            </button>
+          </Link>
+        </div>
+
+      </div>
+
+    </div>
+  )
+};
+
+export default Card;
--- src/core/Home.js
+++ src/core/Home.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from './Layout';
+import Card from './Card';
+import { getProducts } from './apiCore';
 
-const Home = () => (
+const Home = () => {
 
-  <Layout title='Home' description='Node React E-Commerce App'>
+  const [productsSold, setProductsBySold] = useState([]);
+  const [productsbyArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductsSold = () => {
+
+    // make API request
+    getProducts('sold')
+    .then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySold(data);
+      }
+    })
+  };
+
+  const loadProductsByArrival = () => {
+
+    // make API request
+    getProducts('createdAt')
+    .then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    })
+  };
+
+  // executed when component mounts and state changes.
+  useEffect(() => {
+    loadProductsSold();
+    loadProductsByArrival();
+  }, []);
+
+ return (
+  <Layout title='Home' description='Node React E-Commerce App' className='container-fluid'>
+    <h2 className='mb-4'>New Arrivals</h2>
+    <div className='row'>
+      {
+        productsbyArrival.map(product => {
+          return <Card key={product._id} product={product} />
+        })
+      }
+    </div>
+
+    <h2 className='mb-4'>Best Sellers</h2>
+    <div className='row'>
+      {
+        productsSold.map(product => {
+          return <Card key={product._id} product={product} />
+        })
+      }
+    </div>
 
   </Layout>
-)
+  );
+};
 
 export default Home;
