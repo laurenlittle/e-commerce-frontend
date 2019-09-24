f060440b46604900a3c08854930d04c5ff95c3cc
laurenlittle <littlelauren016@gmail.com>
2019-09-23 21:01:19 -0400

    create Checkbox component to update/toggle state for categories checked

---
+++ src/core/Checkbox.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+
+const Checkbox = ({categories}) => {
+
+  const [checked, setChecked] = useState([]);
+
+  const handleToggle = category => () => {
+
+    const currentCategoryId = checked.indexOf(category) // return index of the first occurrence OR not found return -1
+    const newCheckedCategoryId = [...checked];
+
+    if(currentCategoryId === -1) { // if currently checked category wasn't in the state - add
+      newCheckedCategoryId.push(category)
+    } else { // remove
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
+    console.log(newCheckedCategoryId);
+    setChecked(newCheckedCategoryId);
+
+  }
+  return categories.map((category) => (
+
+      <li key={category._id} className='list-unstyled'>
+        <input onChange={handleToggle(category._id)} value={checked.indexOf(category._id === -1)} id='category-checkbox' type='checkbox' className='form-check-input' />
+        <label htmlFor='category-checkbox' className='form-check-label'> {category.name} </label>
+      </li>
+  ));
+};
+
+export default Checkbox;
