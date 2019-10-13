630bae4449657dcc8cea4d3954105bc9b55cf709
laurenlittle <littlelauren016@gmail.com>
2019-10-13 11:32:50 -0400

    refactor, add styles, and update Card to display stock/category

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ShowImage from './ShowImage';
+import moment from 'moment';
 
 
 const Card = ({product, showViewProductButton = true}) => {
+
+  const showStock = quantity => {
+    return quantity > 0 ? (
+        <span className='badge badge-primary badge-pill'>In Stock</span>
+      ) : (
+            <span className='badge badge-primary badge-pill'>OUT OF STOCK</span>
+          )
+  }
 
   const showViewButton = showViewProductButton => {
 
@@ -16,26 +25,37 @@ const Card = ({product, showViewProductButton = true}) => {
         </Link>
       )
     )
-  }
+  };
+
+  const showAddToCartButton = () => {
+    return (
+       <Link to='/'>
+          <button className='btn btn-outline-warning mb-2 mt-2'>
+            Add to Cart
+          </button>
+        </Link>
+    );
+  };
 
   return (
 
       <div className='card'>
-        <div className='card-header'>
+        <div className='card-header name'>
           {product.name}
         </div>
         <div className='card-body'>
           <ShowImage item={product} url='product' />
-          <p> {product.description.substring(0,50)} </p>
-          <p> {product.price} </p>
+          <p className='lead mt-2'> {product.description.substring(0,50)} </p>
+          <p className='black-10'>${product.price} </p>
+          <p className='black-9'>Category: {product.category && product.category.name}</p>
+          <p className='black-8'>Added: {moment(product.createdAt).fromNow()}</p>
+
+          {showStock(product.quantity)}
+          <br/>
 
           {showViewButton(showViewProductButton)}
+          {showAddToCartButton()}
 
-          <Link to='/'>
-            <button className='btn btn-outline-warning mb-2 mt-2'>
-              Add to Cart
-            </button>
-          </Link>
         </div>
 
       </div>
