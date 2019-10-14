a1406b39477e90da3c114b5894d16734ce79c471
laurenlittle <littlelauren016@gmail.com>
2019-10-14 05:47:52 -0400

    add related products to details page

--- src/core/Product.js
+++ src/core/Product.js
@@ -1,17 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import Layout from './Layout';
-import { read } from './apiCore';
+import { read, listRelated } from './apiCore';
 import Card from './Card';
 
 const Product = props => {
 
   const [product, setProduct] = useState({});
+  const [relatedProducts, setRelatedProducts] = useState([]);
   const [error, setError] = useState(false);
 
   useEffect(() => {
     const productId = props.match.params.productId;
     loadSingleProduct(productId);
-  }, []);
+  }, [props]);
 
   // get product Id from route params when component mounts
   const loadSingleProduct = productId => {
@@ -21,7 +22,17 @@ const Product = props => {
       if(data.error) {
         setError(data.error);
       } else {
-        setProduct(data)
+        setProduct(data);
+
+        // fetch related products
+        listRelated(data._id)
+        .then(data => {
+          if(data.error) {
+            setError(data.error);
+          } else {
+            setRelatedProducts(data);
+          }
+        })
       }
     })
   };
@@ -36,11 +47,28 @@ const Product = props => {
     }
     className='container-fluid'>
     <div className='row'>
-      {
-        product &&
-        product.description &&
-      <Card product={product} showViewProductButton={false} />
-      }
+      <div className='col-xs-12 col-sm-8'>
+        {
+          product &&
+          product.description &&
+          <Card product={product} showViewProductButton={false} />
+        }
+      </div>
+      <div className='col-xs-12 col-sm-4'>
+        <h4>Related Products</h4>
+        {
+          relatedProducts.map(product => (
+            <div className='mb-3'>
+              <Card
+                key={product._id}
+                product={product}
+                showViewProductButton={true}  />
+            </div>
+          ))
+        }
+
+
+      </div>
     </div>
   </Layout>
   );
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -76,3 +76,15 @@ export const read = productId => {
       console.log(error);
     })
 };
+
+export const listRelated = productId => {
+  return fetch(`${API}/products/related/${productId}`, {
+      method: 'GET',
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    })
+};
