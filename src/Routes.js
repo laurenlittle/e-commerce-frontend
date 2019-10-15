583fce8bcc3afd86de66fb041912141623aea63c
laurenlittle <littlelauren016@gmail.com>
2019-10-15 06:00:12 -0400

    add Cart to Routes and create method to get cart items

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Signup from '../src/user/Signup';
 import Signin from '../src/user/Signin';
 import Home from '../src/core/Home';
 import Shop from '../src/core/Shop';
+import Cart from '../src/cart/Cart';
 import Product from '../src/core/Product';
 import PrivateRoute from './auth/PrivateRoute';
 import UserDashboard from './user/UserDashboard';
@@ -22,6 +23,7 @@ const Routes = () => {
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/shop" exact component={Shop} />
+        <Route path="/cart" exact component={Cart} />
         <Route path="/product/:productId" exact component={Product} />
         <PrivateRoute path="/user/dashboard" exact component={UserDashboard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
--- src/cart/helpers/cartHelpers.js
+++ src/cart/helpers/cartHelpers.js
@@ -28,4 +28,12 @@ export const getItemTotal = () => {
       return JSON.parse(localStorage.getItem('cart')).length
   }
   return 0;
+};
+
+export const getCart = () => {
+  if (typeof window !== 'undefined') {
+    if(localStorage.getItem('cart'))
+      return JSON.parse(localStorage.getItem('cart'));
+  }
+  return [];
 }
