5b1bcac94c0bd76c7fee67c692a1ecf29d4b5f35
laurenlittle <littlelauren016@gmail.com>
2019-10-17 07:09:58 -0400

    create updateCart helper method to adjust item quantity

--- src/cart/helpers/cartHelpers.js
+++ src/cart/helpers/cartHelpers.js
@@ -36,4 +36,22 @@ export const getCart = () => {
       return JSON.parse(localStorage.getItem('cart'));
   }
   return [];
+};
+
+export const updateItem = (productId, count) => {
+  let cart = [];
+  if (typeof window !== 'undefined') {
+    // access local storage
+    if (localStorage.getItem('cart')) {
+      cart = JSON.parse(localStorage.getItem('cart'));
+    }
+
+    cart.map((product, i) => {
+      if(product._id === productId) {
+        cart[i].count = count
+      }
+    })
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
 }
