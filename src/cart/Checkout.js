853cb5f2b1a80681e6fe32f708c08b8e57757b94
laurenlittle <littlelauren016@gmail.com>
2019-10-22 07:01:20 -0400

    create state, create method to make API request for clientToken, show DropIn UI in Checkout component

--- src/cart/Checkout.js
+++ src/cart/Checkout.js
@@ -1,11 +1,44 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { Link } from 'react-router-dom';
 import Card from '../core/Card';
 import { getCart } from './helpers/cartHelpers';
 import { isAuthenticated } from '../auth';
+import { getBraintreeClientToken } from './apiCart';
+import DropIn from "braintree-web-drop-in-react";
 
 const Checkout = ({products}) => {
+
+  const [data, setData] = useState({
+    success: false,
+    clientToken: null,
+    error: '',
+    instance: {}, // for drop-in UI
+    address: ''
+  });
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  useEffect(() => {
+    setClientToken(userId, token);
+  }, []);
+
+  const setClientToken = (userId, token) => {
+    // make API request
+    getBraintreeClientToken(userId, token)
+    .then(data => {
+      if(data.error) {
+        setData({...data, error: data.error, });
+      } else {
+        setData({
+          ...data,
+          success: true,
+          clientToken: data.clientToken
+        })
+      }
+    })
+  };
 
   const getTotal = () => {
     return products.reduce((acc, curr) => {
@@ -15,9 +48,9 @@ const Checkout = ({products}) => {
 
   const showCheckout = () => {
     return isAuthenticated() ? (
-      <button className='btn btn-success'>
-        Checkout
-      </button>
+      <>
+        {showDropIn()}
+      </>
     ) : (
       <Link to='/signin'>
         <button className='btn btn-primary'>
@@ -26,6 +59,23 @@ const Checkout = ({products}) => {
       </Link>
       )
   };
+
+  const showDropIn = () => (
+
+    <div>
+      {data.clientToken !== null && products.length > 0 ? (
+        <div>
+          <DropIn
+            options={{ authorization: data.clientToken }}
+            onInstance={instance => (data.instance = instance)}
+          />
+          <button className='btn btn-success'>
+            Checkout
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
 
   return(
     <div>
