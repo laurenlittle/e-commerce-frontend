27b19140fb2018317250ba2f1d136be5654b1e5b
laurenlittle <littlelauren016@gmail.com>
2019-10-26 08:49:06 -0400

    create processPayment method to call API

--- src/cart/apiCart.js
+++ src/cart/apiCart.js
@@ -16,3 +16,21 @@ export const getBraintreeClientToken = (userId, userToken) => {
       console.log(error);
     })
 };
+                                                      // Payment Method and Total
+export const processPayment = (userId, userToken, paymentData) => {
+  return fetch(`${API}/braintree/payment/${userId}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        "Content-Type": 'application/json',
+        Authorization: `Bearer ${userToken}`
+      },
+      body: JSON.stringify(paymentData)
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => {
+      console.log('processPaymentError', error);
+    })
+};
