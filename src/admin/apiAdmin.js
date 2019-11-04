e9c763cde8dbc24f18e1c934cfbbc17fd6368b30
laurenlittle <littlelauren016@gmail.com>
2019-11-04 06:46:44 -0500

    add CRUD methods to apiAdmin for Products

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -99,3 +99,64 @@ export const updateOrderStatus = (userId, userToken, orderId, status) => {
     })
 };
 
+// Product CRUD
+
+export const getAllProducts = () => {
+  return fetch(`${API}/products?limit=100`, {
+      method: 'GET',
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    })
+};
+
+export const getSingleProduct = productId => {
+  return fetch(`${API}/product/${productId}`, {
+      method: 'GET',
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    })
+};
+
+export const updateProduct = (productId, userId, userToken, productData) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        "Content-Type": 'application/json',
+        Authorization: `Bearer ${userToken}`
+      },
+      body: productData // sent as form data (not JSON)
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    })
+};
+
+export const deleteProduct = (productId, userId, userToken) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        "Content-Type": 'application/json',
+        Authorization: `Bearer ${userToken}`
+      }
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    })
+};
+
